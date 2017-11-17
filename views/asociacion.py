7f6321e13d0d1422e22e29c7717ba07d694695b0
Pepe Valdivia <jvaldivia@softweb.pe>
2017-11-17 08:42:59 -0500

    Buscar autocompletado responsable

--- views/asociacion.py
+++ views/asociacion.py
@@ -4,7 +4,7 @@
 import json
 from flask import Blueprint, request
 from sqlalchemy.sql import select
-from configs.models import Asociacion
+from configs.models import Asociacion, VWNombreCompletoResponsable as VW
 from configs.database import engine, session_db
 
 asociacion = Blueprint('asociacion', __name__)
@@ -17,4 +17,10 @@ def listar():
 				FROM asociaciones A INNER JOIN responsables R
 				ON A.responsable_id = R.id
         """
+	return json.dumps([dict(r) for r in conn.execute(stmt)])
+
+@asociacion.route('/asociacion/buscar', methods=['GET'])
+def buscar():
+	conn = engine.connect()
+	stmt = select([VW]).where(VW.responsable.like(request.args.get('responsable') + '%' )).limit(10)
 	return json.dumps([dict(r) for r in conn.execute(stmt)])
